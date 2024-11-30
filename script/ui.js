5c259c0d1e5a592fb8dc24070da7759411bdc857
Yigit Bayazit <ygtbyzt@gmail.com>
2024-11-30 12:58:19 +0000

    ui file updated

--- script/ui.js
+++ script/ui.js
@@ -13,3 +13,56 @@ UI.prototype.addTaskToUI = function(newTask){
     </tr>
     `
 }
+
+UI.prototype.clearInputs = function(element1, element2, element3){
+    element1.value = "";
+    element2.value = "";
+    element3.value = "";
+}
+
+UI.prototype.displayMessages = function(type, message){
+    const cardBody = document.querySelector(".card-body");
+    const div = document.createElement("div");
+    div.className = `alert alert-${type}`;
+    div.textContent = message;
+    div.style.opacity = 0;
+    div.style.transition = "opacity 1s";
+    cardBody.appendChild(div);
+
+    setTimeout(function(){
+        div.style.opacity = 1;
+    }, 10)
+
+    setTimeout(function(){
+        div.style.opacity = 0;
+        setTimeout(function(){
+            div.remove();
+        }, 1000)
+    }, 2000)
+}
+
+UI.prototype.loadAllTasks = function(tasks){
+    const taskList = document.querySelector("#tasks");
+    tasks.forEach(function(task){
+        taskList.innerHTML +=`
+        <tr>
+            <td>${newTask.name}</td>
+            <td>${newTask.details}</td>
+            <td>
+                <a href="#" class="btn btn-danger" id="delete-task">Delete</a>
+            </td>
+        </tr>
+        `
+    })
+}
+
+UI.prototype.deleteTaskFromUI = function(delElement){
+    delElement.parentElement.parentElement.remove();
+}
+
+UI.prototype.clearAllTasksFromUI = function(){
+    const taskList = document.querySelector("#tasks")
+    while(taskList.firstChild != null){
+        taskList.removeChild(taskList.firstChild)
+    }
+}
